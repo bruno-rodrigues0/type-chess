6208b9f1713e1bfe387029fae03045e87ff53034
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-20 18:00:42 -0300

    select piece

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -2,3 +2,12 @@ import { filler, squares } from "./utils.js";
 
 filler();
 
+squares.forEach(item => {
+    item.addEventListener('click', (evt) => {
+        for(let i = 0; i < 64; i++){
+            squares[i].classList.remove('select');
+        }
+
+        item.classList.toggle('select');
+    })
+})
